86ee38b43e611b64b2847c8cf0fe72ce8320f514
unknown <pjh00032@gmail.com>
2021-08-24 12:59:26 +0900

    src modify

--- src/Component/HeaderComponent.js
+++ src/Component/HeaderComponent.js
@@ -3,14 +3,14 @@ const HeaderComponent = (props) =>{
     return(
         <header>
             <span className="header-logo">
-                <img src="/images/sparwk.PNG"></img>
+                <img src="./images/sparwk.PNG"></img>
             </span>
             <nav className="header-nav">
                 <ul>
                     <li><button>Log in/Sign up for free</button></li>
-                    <li><a>About Services</a></li>
-                    <li><a>Partners</a></li>
-                    <li><a>Contact</a></li>
+                    <li><a href="/">About Services</a></li>
+                    <li><a href="/">Partners</a></li>
+                    <li><a href="/">Contact</a></li>
                     <li>
                         <label className="switch-button">
                             <input type="checkbox"/> 
